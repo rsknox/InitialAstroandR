c1e54350f933e8323b9fe82c33d3392a146a6ef2
rsknox <rsknox@gmail.com>
2014-07-23 09:05:17 -0500

    Initial commit to familiarize myself

---
+++ Master.R
@@ -0,0 +1,4 @@
+# This is the Master Test script
+# I will create additional function script files and call from this master 
+# program
+# This is an added line, will try a second commit and push ...
